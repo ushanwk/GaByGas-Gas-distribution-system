e68822be14e6cf400701e4861b874e80a8aea76f
UshanKaushalya <Ushanwk22@gmail.com>
2025-01-22 00:01:37 +0530

    App

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/home/HomePage"
-import Customer from "./Pages/customer/Customer"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/customer" element={<Customer />} />
       </Routes>
     </Router>
   )
