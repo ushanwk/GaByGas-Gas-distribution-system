a442e07545aa50df48f09f7e3fe4ebd3859cf26c
UshanKaushalya <Ushanwk22@gmail.com>
2025-01-21 21:27:47 +0530

    Routing added

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/HomePage"
+import LoginPage from "./pages/login/LoginPage"
+import RegisterPage from "./pages/register/RegisterPage"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </Router>
   )
