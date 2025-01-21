7bb61b4d6e066f956b812a40b432e323619fe1e5
ImanAdithya <adithyaiman07@gmail.com>
2025-01-21 21:51:38 +0530

    Added  login component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,13 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/home/HomePage"
+import HomePage from "./Pages/Home/HomePage"
+import LoginPage from "./Pages/login/LoginPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   )
--- front-end/src/common/components/button/Button.jsx
+++ front-end/src/common/components/button/Button.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import PropTypes from "prop-types";
 import Button from "@mui/material/Button";
 import { Link as RouterLink } from "react-router-dom";
 
-function ButtonComponent({ label, onClick, to, variant = "contained", color = "primary", size = "medium", startIcon, endIcon, disabled = false }) {
+function ButtonComponent({
+  label,
+  onClick,
+  to,
+  variant = "contained",
+  color = "primary",
+  size = "medium",
+  startIcon,
+  endIcon,
+  disabled = false,
+  customColor, // Custom color
+  customWidth, // Custom width
+  customHeight, // Custom height
+}) {
   return to ? (
     <Button
       component={RouterLink}
@@ -13,12 +26,16 @@ function ButtonComponent({ label, onClick, to, variant = "contained", color = "p
       startIcon={startIcon}
       endIcon={endIcon}
       disabled={disabled}
-      sx={{ margin: "10px" }}
+      sx={{
+        margin: "10px",
+        backgroundColor: customColor || undefined,
+        width: customWidth || "auto", // Apply custom width if provided
+        height: customHeight || "auto", // Apply custom height if provided
+      }}
     >
       {label}
     </Button>
   ) : (
-
     <Button
       variant={variant}
       color={color}
@@ -27,11 +44,54 @@ function ButtonComponent({ label, onClick, to, variant = "contained", color = "p
       startIcon={startIcon}
       endIcon={endIcon}
       disabled={disabled}
-      sx={{ margin: "10px" }}
+      sx={{
+        margin: "10px",
+        backgroundColor: customColor || undefined,
+        width: customWidth || "auto", // Apply custom width if provided
+        height: customHeight || "auto", // Apply custom height if provided
+      }}
     >
       {label}
     </Button>
   );
 }
+
+ButtonComponent.propTypes = {
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  to: PropTypes.string,
+  variant: PropTypes.oneOf(["text", "outlined", "contained"]),
+  color: PropTypes.oneOf([
+    "default",
+    "inherit",
+    "primary",
+    "secondary",
+    "success",
+    "error",
+    "info",
+    "warning",
+  ]),
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  startIcon: PropTypes.node,
+  endIcon: PropTypes.node,
+  disabled: PropTypes.bool,
+  customColor: PropTypes.string, // Custom color prop
+  customWidth: PropTypes.string, // Custom width prop
+  customHeight: PropTypes.string, // Custom height prop
+};
+
+ButtonComponent.defaultProps = {
+  onClick: null,
+  to: null,
+  variant: "contained",
+  color: "primary",
+  size: "medium",
+  startIcon: null,
+  endIcon: null,
+  disabled: false,
+  customColor: null,
+  customWidth: null,
+  customHeight: null,
+};
 
 export default ButtonComponent;
