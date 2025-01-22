a9f6bb85f58b4b026b77c41b00e560aedf1cd6ab
ImanAdithya <adithyaiman07@gmail.com>
2025-01-22 22:42:52 +0530

    update routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 import HomePage from "./pages/home/HomePage/"
 import LoginPage from "./pages/login/LoginPage/"
@@ -6,12 +5,8 @@ import RegisterPage from "./pages/register/RegisterPage"
 import Customer from "./Pages/customer/Customer";
 import HeadOfficePage from "./Pages/head-office/HeadOfficePage";
 import OutletPage from "./Pages/outlet/OutletPage";
-=======
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./Pages/Home/HomePage"
-import LoginPage from "./Pages/login/LoginPage";
-import Register from "./Pages/register/RegisterPage";
->>>>>>> origin/dev-sathin
+import Register from "./Pages/register/RegisterPage/";
+
 
 function App() {
   return (
@@ -19,14 +14,11 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-<<<<<<< HEAD
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/customer" element={<Customer/>} />
         <Route path="/head" element={<HeadOfficePage/>} />
         <Route path="/outlet" element={<OutletPage/>} />
-=======
         <Route path="/register" element={<Register />} />
->>>>>>> origin/dev-sathin
       </Routes>
     </Router>
   )
