7d61dbc2c22b5f585ecfc1010dcf524ab9233ac8
adithya <adithyaiman07@gmail.com>
2025-02-19 10:44:40 +0530

    opt send in mail

--- back-end/controller/SendMailController.js
+++ back-end/controller/SendMailController.js
@@ -21,7 +21,7 @@ const sendMail = async (req, res) => {
   
     try {
       const info = await transporter.sendMail(mailOptions);
-      res.status(200).json({ message: 'Email sent', info });
+      res.status(200).json({ message: 'Email sent', otp });
     } catch (error) {
       console.error('Error sending email:', error);
       res.status(500).json({ message: 'Error sending email', error: error.message });
