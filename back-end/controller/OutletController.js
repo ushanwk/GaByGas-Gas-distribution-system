5eda3747ece58e47581261fe428dcd65925900c5
UshanKaushalya <Ushanwk22@gmail.com>
2025-03-03 21:38:15 +0530

    Adding

--- back-end/controller/OutletController.js
+++ back-end/controller/OutletController.js
@@ -25,7 +25,7 @@ const getOutlet = async (req, res) => {
 const addOutlet = async (req, res) => {
   const { Outlet_Id, Location } = req.body;
   try {
-    const outlet = await Outlet.create({ Outlet_Id, Location });
+    const outlet = await Outlet.create({ Outlet_Id, Location, Name });
     res.status(201).json(outlet);
   } catch (error) {
     res.status(400).json({ error: "Failed to create outlet" });
--- back-end/model/Outlet.js
+++ back-end/model/Outlet.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const OutletSchema = new mongoose.Schema({
     Outlet_Id: { type: String, required: true, unique: true },
-    Location: { type: String, required: true }
+    Location: { type: String, required: true },
+    Name: { type: String, required: true }
 })
 
 module.exports = mongoose.model('Outlet', OutletSchema);
